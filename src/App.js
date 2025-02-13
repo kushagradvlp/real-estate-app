8956486e38b28e8d8a44530d1456f2470e3d00de
Kushagra <kushagragupta97@gmail.com>
2025-02-12 23:26:10 -0500

    Chatbot page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Navbar, Home, Listings } from "./components/Navbar";
 import PropertyDetails from "./components/PropertyDetails";
+import Chatbot from "./components/Chatbot";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/listings" element={<Listings />} />
         <Route path="/property/:id" element={<PropertyDetails />} />
-        <Route path="/chatbot" element={<h1>Chatbot Page</h1>} />
+        <Route path="/chatbot" element={<Chatbot />} />
         <Route path="/dashboard" element={<h1>Dashboard</h1>} />
         <Route path="/login" element={<h1>Login Page</h1>} />
       </Routes>
